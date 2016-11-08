f446f903e124403bd982880623c0b111bbc1e405
Tanghaiyan <584536319@qq.com>
2016-11-08 23:49:54 +0800

    event todo

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 $(function(){
-   //$('.container').click(function(){
+   //dom加载完成后使用加载动画
    $('.layer').css("width","10%").css("height","5%");
    $('.layer').animate({height:"40%"},100)
    .animate({height:"60%"},200)
    .animate({height:"80%",width:"80%"},500,function(){
-   		console.log(1);
    		$('.content').css("display","block");
-   		//$('.company').css("transform","translateX(-20px)");
-   		$('.company').css("transition","company 500ms ease");
+   		$('.footer').slideDown("slow");
    	});
+
+   $("p").focus(function(e){
+   	//$(this).
+   	e.preventDefault();
+   	$(this).css("outline","none");
+
+   })
 
 
 
